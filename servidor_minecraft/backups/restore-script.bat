00c6dd5ca662fc5c479d153e3e201e7b8781b97d
Todorokii1lol <aaronjeldres441@gmail.com>
2025-01-14 04:36:13 +0000

    [🌙] Backup realizado 2025-01-14

---
+++ servidor_minecraft/backups/restore-script.bat
@@ -0,0 +1,5 @@
+@echo off 
+/:
+cd "/workspaces/lol/servidor_minecraft/mods"
+java -jar "AdvancedBackups-forge-1.19.2-3.6.4.jar"
+pause
---
+++ servidor_minecraft/backups/restore-script.sh
@@ -0,0 +1,2 @@
+cd "/workspaces/lol/servidor_minecraft/mods"
+java -jar "AdvancedBackups-forge-1.19.2-3.6.4.jar"
